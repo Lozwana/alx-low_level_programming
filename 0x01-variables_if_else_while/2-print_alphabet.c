ba4f236b53eeb0731f4d6df5ec774ac39ffc333a
happyness <mabasohappyness@gmail.com>
2023-07-14 07:25:11 -0700

    using putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main -  prints the alphabet in lowercase
+ * using putchar function
+ * Return: 0
+ */
+int main(void)
+{
+	int = 97;
+
+	while (n <= 122)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+
+	return (0);
+}
