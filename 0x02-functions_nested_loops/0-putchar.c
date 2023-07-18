e46a8bd9b7a20b06558e8404e5ce3301b4e06cdd
happyness <mabasohappyness@gmail.com>
2023-07-18 07:00:18 -0700

    Write a program that prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: A program that prints _putchar
+ *
+ * Return : Always 0 (success)
+ */
+int main(void)
+{
+	int = printf
+
+	printf("\"_putchar");
+
+	printf("\n");
+	return (0);
+}
