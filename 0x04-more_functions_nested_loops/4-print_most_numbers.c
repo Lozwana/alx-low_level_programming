c51ade5ff39f99214850ed9a1f27f985c8951e49
happyness <mabasohappyness@gmail.com>
2023-07-20 14:15:57 -0700

    print numbers with _putchar

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Prints the numbers, from 0 to 9
+ * Do not print 2 and 4
+ */
+void print_most_numbers(void)
+{
+	int a = 0;
+
+	for (; a <= 9; a++)
+	{
+	if (a == 2 || a == 4)
+	{
+	continue;
+	}
+	else
+	{
+	_putchar(a + '0');
+	}
+	}
+	_putchar('\n');
+}
