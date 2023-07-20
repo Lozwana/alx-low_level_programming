d21182b50c83ac76c90b848b2073bf2051205770
happyness <mabasohappyness@gmail.com>
2023-07-20 13:22:58 -0700

    _putchar function

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,13 +6,12 @@
  */
 
 void print_numbers(void)
-
 {
-	int x;
+	int a;
 
-	for (x = 0; x <= 9; x++)
+	for (a = 0; a <= 9; a++)
 	{
-		_putchar(x + '0');
+		_putchar('0' + a);
 	}
 	_putchar('\n');
 }
