bf3582c070d46991227aa1ef46b0e79fc874e8db
happyness <mabasohappyness@gmail.com>
2023-07-20 13:06:17 -0700

    _putchar funvtion

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
- * print_numbers - prints the numbers, from 0 to 9
+ * print_numbers - Prints the numbers, from 0 to 9
  * Return: The numbers from 0 to 9
  */
 
 void print_numbers(void)
 
 {
-	int c;
+	int x;
 
-	for (c = 0; c <= 9; c++)
+	for (x = 0; x <= 9; x++)
 	{
-	_putchar(c + '0');
+		_putchar(x + '0');
 	}
 	_putchar('\n');
 }
