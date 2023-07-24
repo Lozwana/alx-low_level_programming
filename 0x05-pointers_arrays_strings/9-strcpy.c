ae1818fccaa2add0936c5c5dbd521908090a5e94
happyness <mabasohappyness@gmail.com>
2023-07-24 16:53:57 -0700

    copy string

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strcpy - copy a string
+ * _strcpy - Copy a string
  * @dest: Destination value
  * @src: Source value
  * Return: the pointer to dest
@@ -10,9 +10,9 @@
 char *_strcpy(char *dest, char *src)
 {
 int i;
-for (i = 0; scr[i] != '\0'; i++)
+for (i = 0; src[i] != '\0'; i++)
 {
-dest[i] = scr[i];
+dest[i] = src[i];
 }
 dest[i++] = '\0';
 return (dest);
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -8,3 +8,4 @@ void _puts(char *str);
 int _strlen(char *s);
 void swap_int(int *a, int *b);
 void reset_to_98(int *n);
+char *_strcpy(char *dest, char *src);
