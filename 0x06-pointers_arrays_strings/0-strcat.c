1847d35ce553141396e30fc468082c2464716b87
happyness <mabasohappyness@gmail.com>
2023-07-26 10:53:07 -0700

    concatenates two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,30 +2,23 @@
 
 /**
  * _strcat - concatenates two strings
- * @dest: input value
- * @src: input value
- * Return: void
+ * @dest: destination string
+ * @src: source string
+ * Return: dest;
  */
 
 char *_strcat(char *dest, char *src)
 
 {
-	int k;
-	int n;
+	int destlen = 0;
+	int srclen = 0;
+	int i;
 
-	k = 0;
-	while (dest[k] != '\0')
-	{
-		k++;
-	}
-	n = 0;
-	while (src[n] != '\0')
-	{
-		dest[k] = src[n];
-		k++;
-		n++;
-	}
-
-	dest[k] = '\0';
+	for (i = 0 ; dest[i] != '\0' ;  i++)
+		destlen++;
+	for (i = 0 ; src[i] != '\0' ; i++)
+		srclen++;
+	for (i = 0 ; i <= srclen ; i++)
+		dest[destlen + i] = src[i];
 	return (dest);
 }
